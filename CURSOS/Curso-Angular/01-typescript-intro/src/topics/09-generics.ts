ac62c7c433a92a6103f951f04a02e7b01196ee33
Dany Flores <danyfloresgarcia18@gmail.com>
2024-06-22 16:17:21 -0500

    Revert "cursos angular"
    This reverts commit 5f6059cf8df5b0402838887b8e1d977695f682f4.

--- CURSOS/Curso-Angular/01-typescript-intro/src/topics/09-generics.ts
+++
@@ -1,9 +0,0 @@
-export function WhatMyTypes<T>(argument: T): T{
-    return argument;
-}
-
-let string = WhatMyTypes<string>("jojo jojo").split(' ');
-let number = WhatMyTypes<number>(20);
-let object = WhatMyTypes<object>({name: "dany"});
-
-console.log({string, number, object});
